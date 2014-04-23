73e89c1d0edf3bf9ad56b76fe8c8e695a9ff4b30
Дмитрий Шаматрин <shamatrin@reg.ru>
2014-04-23 22:31:30 +0300

    geninit

---
+++ lib/System/InitD/GenInit.pm
@@ -0,0 +1,60 @@
+package System::InitD::GenInit;
+
+use strict;
+use warnings;
+use Getopt::Long;
+use Carp;
+
+sub new {
+    my $class = shift;
+
+    return bless {}, $class;
+}
+
+
+sub run {
+    my $self = shift;
+
+    $self->parse_args();
+    
+    if (lc $self->{options}->{os} eq 'debian') {
+        require System::InitD::Debian;
+        System::InitD::Debian::generate($self->{options});
+    }
+    else {
+        croak 'Unknown OS';
+    }
+
+    1;
+};
+
+
+sub parse_args {
+    my $self = shift;
+
+    my $opts = $self->{options} = {
+        author  =>  getlogin,
+        os      =>  'debian',
+    };
+
+    GetOptions(
+        'os=s'            =>  \$opts->{os},
+        'target=s'        =>  \$opts->{target},
+        "author=s"        =>  \$opts->{author},
+        'pid_file=s'      =>  \$opts->{pid_file},
+        'process_name=s'  =>  \$opts->{process_name},
+        'start_cmd=s'     =>  \$opts->{start_cmd},
+    );
+
+    if (scalar @ARGV == 1 && !$self->{options}->{target}) {
+        $self->{options}->{target} = $ARGV[0];
+    }
+
+    return 1;
+}
+
+
+1;
+
+__END__
+
